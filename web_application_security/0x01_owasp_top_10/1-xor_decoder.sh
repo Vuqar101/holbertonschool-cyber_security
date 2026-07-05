147baf731ad8074ca10cc95e6fffe961d0af788b
Vuqar101 <veliyevvuqar10@gmail.com>
2026-07-05 16:10:09 +0000

    salam

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Əgər arqument verilməyibsə çıxış et
 if [ -z "$1" ]; then
-    echo "İstifadə qaydası: $0 {xor}KzosKw=="
+    echo "Usage: $0 <hash>"
     exit 1
 fi
 
-# Başlıqdakı {xor} hissəsini təmizləyirik
-encoded_str="${1#\{xor\}}"
+BASE64_STR="${1#\{xor\}}"
 
-# Base64 ilə deşifrə edib, hər baytı 0x5F (_) ilə XOR edirik
-echo -n "$encoded_str" | base64 -d 2>/dev/null | python3 -c '
-import sys
-data = sys.stdin.buffer.read()
-decoded = "".join(chr(b ^ 0x5F) for b in data)
-print(decoded)
-'
+echo -n "$BASE64_STR" | base64 -d 2>/dev/null | od -An -v -tu1 | awk '
+{
+    for (i=1; i<=NF; i++) {
+        # 95 is the decimal value for 0x5F (_)
+        printf "%c", xor($i, 95)
+    }
+}
+END { print "" }'
